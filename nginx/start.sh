66abf17372e2b22a187c60dc435765fa059e6409
Lawrence Lau <lncknight@gmail.com>
2022-08-04 23:59:12 +0800

    nginx - arm64

---
+++ nginx/start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# htpasswd -b -c /etc/nginx/arena.passwd $ARENA_USER $ARENA_PASSWD
+# htpasswd -b -c /etc/nginx/redis_admin.passwd $REDIS_ADMIN_USER $REDIS_ADMIN_PASSWD
+
+# nginx # nginx -g "daemon off;"
+# echo 'starting NGINX'
+# nginx
+
+# supervisord -c /etc/supervisord.conf
+
+echo 'starting NGINX'
+nginx -g "daemon off;"
